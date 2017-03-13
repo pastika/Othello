b6b80f300d844a05ce3778e06bb12f423b1726e1
Joe Pastika <joe.pastika@gmail.com>
2017-03-13 21:40:28 +0100

    Replace abs with fabs

--- othelloPlayerCES.cpp
+++ othelloPlayerCES.cpp
@@ -17,11 +17,11 @@ void OthelloPlayerCES::returnPlay(const OthelloBoard<8, 8>& board, int& x, int& 
         }
         else if(move.first == 0 || move.first == 7 || move.second == 0 || move.second == 7)
         {
-            plays[2 + std::min(int(abs(3.5 - move.first)), int(abs(3.5 - move.second)))].push_back(move);
+            plays[2 + std::min(int(fabs(3.5 - move.first)), int(fabs(3.5 - move.second)))].push_back(move);
         }
         else
         {
-            plays[5 + 4*std::max(int(abs(3.5 - move.first)), int(abs(3.5 - move.second))) + std::min(int(abs(3.5 - move.first)), int(abs(3.5 - move.second)))].push_back(move);
+            plays[5 + 4*std::max(int(fabs(3.5 - move.first)), int(fabs(3.5 - move.second))) + std::min(int(fabs(3.5 - move.first)), int(fabs(3.5 - move.second)))].push_back(move);
         }
     }
     
